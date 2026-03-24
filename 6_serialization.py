3a85b01bd750fb5f9e2a938feed2d77cc2595da6
Manish Shrivastav <carttoshoppe@gmail.com>
2026-03-24 12:50:29 -0500

    Set default gender for Patient model and enhance serialization output

--- 6_serialization.py
+++ 6_serialization.py
@@ -7,6 +7,20 @@ class Address(BaseModel):
 
 class Patient(BaseModel):
     name: str
-    gender: str
+    gender: str = 'Male'
     age: int
     address: Address
+
+
+address_dict = {"city": "New York", "state": "NY", "zip_code": "10001"}
+
+address1 = Address(**address_dict)
+
+patient_dict = {"name": "John Doe", "age": 30, "address": address1}
+
+patient1 = Patient(**patient_dict)
+
+temp = patient1.model_dump(exclude={"name": True, "age": True, "address": {"zip_code": True}}, exclude_unset=True)
+
+print(temp)
+print(type(temp))
