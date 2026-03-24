763705c78a3de829ca02f844704e60bf1477dd3b
Manish Shrivastav <carttoshoppe@gmail.com>
2026-03-24 11:42:07 -0500

    Enhance Patient model with email and linkedin_url fields; update data handling for optional attributes

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,18 +1,21 @@
-from pydantic import BaseModel
-from typing import Dict, List
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import Dict, List, Optional
 
 class Patient(BaseModel):
     name: str
+    email: EmailStr
+    linkedin_url: Optional[AnyUrl] = None
     age: int
     weight: float = None
-    married: bool = None
-    allergies: List[str] = None
-    contact_details: Dict[str, str] = None
+    married: Optional[bool] = None
+    allergies: Optional[List[str]] = None
+    contact_details: Optional[Dict[str, str]] = None
 
 
 def insert_patient_data(patient: Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.allergies)
     print("Inserting patient data into the database...")
 
 def update_patient_data(patient: Patient):
@@ -21,11 +24,13 @@ def update_patient_data(patient: Patient):
     print("Updating patient data in the database...")
 
 patient_info = {"name": "nitin", 
+                'email': "abc@gmail.com",
                 "age": "30", 
                 "weight": 75.5, 
                 "married": True, 
                 "allergies": ["pollen", "dust"], 
-                "contact_details": {"email": "nitin@example.com", "phone": "1234567890"}}
+                "contact_details": {"email": "nitin@example.com", "phone": "1234567890"},
+                "linkedin_url": "https://www.linkedin.com/in/nitin"}
 
 patient1 = Patient(**patient_info)
 
