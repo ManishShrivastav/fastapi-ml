be85ad74ccc02b9e0d20bb18f91088b88faa2a2c
Manish Shrivastav <carttoshoppe@gmail.com>
2026-03-24 11:52:41 -0500

    Refactor Patient model to use Annotated fields with validation and descriptions

--- 1_pydantic_why.py
+++ 1_pydantic_why.py
@@ -1,14 +1,14 @@
 from pydantic import BaseModel, EmailStr, AnyUrl, Field
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Annotated
 
 class Patient(BaseModel):
-    name: str
+    name: Annotated[str, Field(max_length=50, title="Patient Name", description="The name of the patient in less than 50 characters", example="John Doe")]
     email: EmailStr
     linkedin_url: Optional[AnyUrl] = None
-    age: int
-    weight: float = None
-    married: Optional[bool] = None
-    allergies: Optional[List[str]] = None
+    age: int = Field(..., gt=0, lt=120, description="Age must be a positive integer between 1 and 119")
+    weight: Annotated[float, Field(gt=0, strict=True, description="Weight must be a positive number")]
+    married: Annotated[bool, Field(default=None, description="Indicates if the patient is married")]
+    allergies: Annotated[Optional[List[str]], Field(default=None, max_length=5, description="List of allergies (max 5 items)")]
     contact_details: Optional[Dict[str, str]] = None
 
 
