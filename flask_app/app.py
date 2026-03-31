17dfc9f3e8c382a80ba453fe486f0f01ddc87456
Manish Shrivastav <carttoshoppe@gmail.com>
2026-03-31 09:29:40 -0500

    Add Flask application with multiplication table functionality and HTML templates

---
+++ flask_app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        number = int(request.form['number'])
+        table = [(number, i, number * i) for i in range(1, 11)]
+        return render_template('result.html', number=number, table=table)
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
